16fe3a393297203d6ec09b0ac8a65bc607709c0b
Naresh Ban <naresh.ban@infodevlopers.com.np>
2022-12-09 16:01:06 +0545

    feat: sidebar.

--- src/core/private/Dashboard/Dashboard.tsx
+++ src/core/private/Dashboard/Dashboard.tsx
@@ -1,14 +1,10 @@
-import React from "react"
-import PrimaryNav from "../Navbars/PrimaryNav/PrimaryNav"
 import SecondaryNav from "../Navbars/Sidebars/SecondaryNav"
 
 const Dashboard = () => {
   return (
     <>
-      <PrimaryNav />
-      <div className='flex w-100 flex-grow-1'>
         <SecondaryNav />
-      </div>
+ 
     </>
   )
 }
